d15b197f9e81689fb663c113455533a8011e5acd
Juan Jose Fidalgo Moran <jjfidalgo@gmail.com>
2012-05-18 13:26:03 +0200

    Primer commit

---
+++ src/main/java/es/paradigma/App.java
@@ -0,0 +1,13 @@
+package es.paradigma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
